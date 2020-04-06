70ec4fefe8e687b03e69a97fe0d4e1aa5d32ddc8
tyort <neotyo1986@gmail.com>
2020-04-06 15:55:07 +0300

    меняй-удаляй

--- src/controllers/item-controller.js
+++ src/controllers/item-controller.js
@@ -1,14 +1,28 @@
 import CardListItemComponent from '../components/card_list_item.js';
 import CardListItemFormComponent from '../components/card_list_item_form.js';
-import {renderComponent, replace} from '../formulas.js';
+import {renderComponent, replace, remove} from '../formulas.js';
 
-const MODE = {
+export const MODE = {
+  ADDING: `adding`,
   DEFAULT: `default`,
   FORM: `form`,
 };
 
+export const EmptyPoint = {
+  id: ``,
+  extraOptions: new Map(),
+  waybillType: ``,
+  waybillPurpose: ``,
+  description: new Set(),
+  photos: new Set(),
+  cardItemDate: [],
+  spendingTime: [],
+  price: 0,
+  isFavorite: false,
+};
+
 export default class ItemController {
-  constructor(container, onDataChange, onViewChange) {
+  constructor(container, onDataChange, onViewChange, place) {
     this._container = container;
     this._onDataChange = onDataChange;
     this._onViewChange = onViewChange;
@@ -17,11 +31,13 @@ export default class ItemController {
     this._cardListItemFormComponent = null;
     this._onEscKeyDown = this._onEscKeyDown.bind(this);
     this._onButtonClick = this._onButtonClick.bind(this);
+    this._place = place;
   }
 
-  renderCardItem(cardItem) { // рендер для одной карточки
+  renderCardItem(cardItem, mode) { // рендер для одной карточки
     const oldCardListItemComponent = this._cardListItemComponent;
     const oldCardListItemFormComponent = this._cardListItemFormComponent;
+    this.mode = mode;
     this._cardListItemComponent = new CardListItemComponent(cardItem);
     this._cardListItemFormComponent = new CardListItemFormComponent(cardItem);
 
@@ -34,14 +50,29 @@ export default class ItemController {
 
     this._cardListItemFormComponent.setSubmitHandler((evt) => {
       evt.preventDefault();
-      this._replaceFormToItem();
+      const data = this._cardListItemFormComponent.getChangedDataByView();
+      this._onDataChange(this, cardItem, data);
     });
 
-    if (oldCardListItemComponent && oldCardListItemFormComponent) {
-      replace(this._cardListItemFormComponent, oldCardListItemFormComponent);
-      replace(this._cardListItemComponent, oldCardListItemComponent);
-    } else {
-      renderComponent(this._container, this._cardListItemComponent);
+    switch (mode) {
+      case MODE.DEFAULT:
+        if (oldCardListItemComponent && oldCardListItemFormComponent) {
+          replace(this._cardListItemFormComponent, oldCardListItemFormComponent);
+          replace(this._cardListItemComponent, oldCardListItemComponent);
+        } else {
+          renderComponent(this._container, this._cardListItemComponent, this._place);
+        }
+        break;
+      case MODE.ADDING:
+        if (oldCardListItemComponent && oldCardListItemFormComponent) {
+          remove(oldCardListItemComponent);
+          remove(oldCardListItemFormComponent);
+        }
+        document.addEventListener(`keydown`, this._onEscKeyDown);
+        this._cardListItemFormComponent.getElement().querySelector(`.event__rollup-btn`)
+          .addEventListener(`click`, this._onButtonClick);
+        renderComponent(this._container, this._cardListItemFormComponent, this._place);
+        break;
     }
   }
 
@@ -49,6 +80,14 @@ export default class ItemController {
     if (this._mode !== MODE.DEFAULT) { // если MODE.FORM
       this._replaceFormToItem(); // превратить форму в карточку
     }
+  }
+
+  destroy() {
+    remove(this._cardListItemFormComponent);
+    remove(this._cardListItemComponent);
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+    this._cardListItemFormComponent.getElement().querySelector(`.event__rollup-btn`)
+      .removeEventListener(`click`, this._onButtonClick);
   }
 
   _replaceFormToItem() {
@@ -67,6 +106,9 @@ export default class ItemController {
 
   _onEscKeyDown(evt) {
     if (evt.key === `Escape` || evt.key === `Esc`) {
+      if (this._mode === MODE.ADDING) {
+        this._onDataChange(this, EmptyPoint, null);
+      }
       this._replaceFormToItem();
     }
   }
