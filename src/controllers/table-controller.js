8d0b25ae121e910624fd784059f1943b1fbcadaa
tyort <neotyo1986@gmail.com>
2020-04-27 22:03:52 +0300

    закрытие открытой формы при нажатии NEW EVENT

--- src/controllers/table-controller.js
+++ src/controllers/table-controller.js
@@ -116,6 +116,7 @@ export default class TableController {
     }
 
     hideComponent(`trip-events__msg`);
+    this._onViewChange();
     this._creatingPoint = new ItemController(this._assortmentComponent.getElement(), this._onDataChange, this._onViewChange, externalBase);
     this._creatingPoint.renderCardItem(EmptyPoint, Mode.ADDING);
   }
